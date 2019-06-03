bb2bb79fa576ff9af9100c27e4cb3f3b7d2923ea
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-03 10:41:53 -0400

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,71 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="grid">
+      <div @click="activate(0,0)" :class="{ off: !isOn, on: isOn }"></div>
+      <div @click="activate()"></div>
+      <div @click="activate()"></div>
+
+      <div @click="activate()"></div>
+      <div @click="activate()"></div>
+      <div @click="activate()"></div>
+
+      <div @click="activate()"></div>
+      <div @click="activate()"></div>
+      <div @click="activate()"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    };
+  },
+  methods: {
+    activate(x,y) {
+      // this.board[x][y] = this.board[x][y] === 1 ? 0 : 1;
+      this.isOn = this.board[0][0] == 1 ? true : false;
+      // console.log('off ', this.board[0][0] === 0);
+      // console.log('on ', this.board[0][0] === 1);
+    }
+  },
+  computed: {
+    isOn() {
+      return this.board[0][0] === 1;
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+  display: flex;
+  justify-content: center;
 
-h1, h2 {
-  font-weight: normal;
-}
+  .grid {
+    display: grid;
+    grid-template-rows: 100px 100px 100px;
+    grid-template-columns: 100px 100px 100px;
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    div {
+      border: 1px solid;
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+      &.off {
+        background-color: green;
+      }
+      &.on {
+        background-color: #27f527;
+      }
+    }
+  }
 }
 </style>
