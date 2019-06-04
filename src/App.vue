b5bcd85386497b8ebbffbe46be66c77cf716a7da
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-04 10:59:41 -0400

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <div class="grid">
-      <div v-for="(row, x) in board" :key="row.id" class="row">
+      <div v-for="(row, row_x) in board" :key="row_x.id" class="row">
         <div
-          v-for="(col, y) in board"
-          :key="col.id"
+          v-for="(col, col_y) in board"
+          :key="col_y.id"
           class="col"
-          @click="activate(x,y)"
-          :class="{off: !gridOn, on: gridOn}"
+          @click="activate(row_x,col_y)"
+          :class="{off: !gridOn && row_x != x && col_y != y, on: gridOn && row_x == x && col_y == y}"
         ></div>
       </div>
     </div>
@@ -26,9 +26,9 @@ export default {
     };
   },
   methods: {
-    activate(x, y) {
-      this.x = x;
-      this.y = y;
+    activate(row_x, col_y) {
+      this.x = row_x;
+      this.y = col_y;
     }
   },
   computed: {
@@ -36,6 +36,7 @@ export default {
     gridOn() {
       console.log("gridon", "x: ", this.x, "y:", this.y);
       if (this.x || this.y) {
+        console.log(this.x, this.y, this.$store.getters.isOn({ row: this.x, col: this.y }))
         return this.$store.getters.isOn({ row: this.x, col: this.y });
       } else {
         return false;
