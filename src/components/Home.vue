7bcffe2f794383a02185f513d8399c86fadf26ed
Dana <dana.ottaviani@gmail.com>
2020-02-19 11:19:05 -0500

    feat: create Home page

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1 class="display-1 mb-3">Choose a Level</h1>
+    <v-btn class="level1" @click="changeLevel(1)">Level 1</v-btn>
+    <v-btn class="level2" @click="changeLevel(2)">Level 2</v-btn>
+    <v-btn class="level3" @click="changeLevel(3)">Level 3</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      level: 0
+    }
+  },
+  watch: {
+    level () {
+      // console.log(this.level)
+      // this.$emit('levelSelected');
+    }
+  },
+  methods: {
+    changeLevel(level) {
+      this.level = level;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.level1 {
+  background-color: #885888;
+  color: white;
+}
+.level2 {
+  background-color: #764076;
+  color: white;
+}
+.level3 {
+  background-color: #662866;
+  color: white;
+}
+</style>
